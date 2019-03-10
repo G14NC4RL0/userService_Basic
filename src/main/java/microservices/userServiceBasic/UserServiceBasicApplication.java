ab14cde70e1e15fea9f9ba6bf8940c1e2bf867cb
Giancarlo <gtedesco@gmail.com>
2019-03-10 18:09:34 +0000

    JPA, DB loader and interface created

--- src/main/java/microservices/userServiceBasic/UserServiceBasicApplication.java
+++ src/main/java/microservices/userServiceBasic/UserServiceBasicApplication.java
@@ -1,7 +1,16 @@
 package microservices.userServiceBasic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
 
 @SpringBootApplication
 public class UserServiceBasicApplication {
@@ -10,4 +19,92 @@ public class UserServiceBasicApplication {
 		SpringApplication.run(UserServiceBasicApplication.class, args);
 	}
 
+}
+
+@Component
+class UserCLR implements CommandLineRunner {
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		for (User u : this.userRepository.findAll()) {
+			System.out.println(u.toString());
+		}
+	}
+
+	@Autowired UserRepository userRepository;
+}
+
+interface UserRepository extends JpaRepository<User, Long> {
+
+	Collection<User> findByLastName(String lastName);
+}
+
+@Entity
+class User {
+
+	private @Id @GeneratedValue long id;
+	private String name;
+	private String lastName;
+	private String email;
+	private String address;
+	private String nationality;
+
+	public User() {
+	}
+
+	public User(String name, String lastName) {
+		super();
+		this.name = name;
+		this.lastName = lastName;
+		this.email = "-";
+		this.address = "-";
+		this.nationality = "-";
+	}
+
+//	public User(long id, String name, String lastName, String email, String address, String nationality) {
+//		this.id = id;
+//		this.name = name;
+//		this.lastName = lastName;
+//		this.email = email;
+//		this.address = address;
+//		this.nationality = nationality;
+//	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", lastName='" + lastName + '\'' +
+				", email='" + email + '\'' +
+				", address='" + address + '\'' +
+				", nationality='" + nationality + '\'' +
+				'}';
+	}
 }
