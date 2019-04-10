0ce351e9c2662143773103fd91012b2e8aebf47d
Giancarlo <gtedesco@gmail.com>
2019-04-10 19:57:08 +0100

    Other tests

--- src/test/java/microservices/userservicebasic/domain/UserServiceTest.java
+++ src/test/java/microservices/userservicebasic/domain/UserServiceTest.java
@@ -1,5 +1,6 @@
 package microservices.userservicebasic.domain;
 
+import microservices.userservicebasic.domain.exception.UserNotFoundException;
 import microservices.userservicebasic.domain.mapper.UserMapper;
 import microservices.userservicebasic.domain.model.User;
 import microservices.userservicebasic.domain.model.UserDetails;
@@ -8,43 +9,64 @@ import microservices.userservicebasic.repository.UserRepository;
 import microservices.userservicebasic.repository.model.UserDto;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 
 
+	private static final UUID USER_UUID = UUID.randomUUID();
 	private static final String FIRST_NAME = "Gian";
 	private static final String LAST_NAME = "Tedesco";
 	private static final String EMAIL = "giancarlo@gmail.com";
 
+	@Mock
 	private EmailValidation emailValidation;
+
+	@Mock
 	private UserRepository userRepository;
-	// What to do in this case?
+
+	@Mock
 	private UserMapper userMapper;
+
+	@InjectMocks
 	private UserService userService;
 
 	@Before
-	public void setUp() throws Exception {
-		emailValidation = new EmailValidation();
-		userMapper = new UserMapper();
-		userService = new UserService(emailValidation, userRepository, userMapper);
+	public void setUp() {
 	}
 
 	@Test
 	public void saveUserWithValidUserDetails() {
 		// Given
 		UserDetails userDetails = new UserDetails(FIRST_NAME, LAST_NAME, EMAIL);
+		UserDto userDto = new UserDto(USER_UUID, FIRST_NAME, LAST_NAME, EMAIL);
+		User expectedUser = new User(USER_UUID, FIRST_NAME, LAST_NAME, EMAIL);
 
 		// When
-		User expectedUser = userService.saveUser(userDetails);
+		Mockito.when(userMapper.mapToUserDto(Mockito.any(User.class)))
+				.thenReturn(userDto);
+		Mockito.when(userRepository.save(userDto))
+				.thenReturn(userDto);
+		Mockito.when(userMapper.mapToUser(userDto))
+				.thenReturn(expectedUser);
+
+		User resultUser = userService.saveUser(userDetails);
 
 		// Then
-		assertEquals(expectedUser.getName(), userDetails.getFirstName());
-		assertEquals(expectedUser.getLastName(), userDetails.getLastName());
-		assertEquals(expectedUser.getEmail(), userDetails.getEmail());
+		assertEquals(expectedUser, resultUser);
+		Mockito.verify(emailValidation, Mockito.times(1))
+				.isValidEmailAddress(EMAIL);
 	}
 
 	@Test
@@ -52,13 +74,31 @@ public class UserServiceTest {
 		// Given
 		UUID userUuid = UUID.randomUUID();
 		UserDto userDto = new UserDto(userUuid, FIRST_NAME, LAST_NAME, EMAIL);
-		userRepository.save(userDto);
 		User expectedUser = new User(userUuid, FIRST_NAME, LAST_NAME, EMAIL);
 
 		// When
+		Mockito.when(userRepository.findById(userUuid))
+				.thenReturn(Optional.of(userDto));
+		Mockito.when(userMapper.mapToUser(userDto))
+				.thenReturn(expectedUser);
+
 		User userResult = userService.getUser(userUuid);
 
 		// Then
 		assertEquals(expectedUser, userResult);
+	}
+
+	@Test
+	public void getUserWithInvalidDataThrowsException() {
+		// Given
+		UUID userUuid = UUID.randomUUID();
+
+		// When
+		Mockito.when(userRepository.findById(userUuid))
+				.thenReturn(Optional.empty());
+
+		assertThatExceptionOfType(UserNotFoundException.class)
+				.isThrownBy(() -> userService.getUser(userUuid))
+				.withMessage("User ID: " + userUuid + " not found");
 	}
 }
